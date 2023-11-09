e8b2f4efe9af2d63338a34faf56a9004bd492ab7
Baghirov Feyruz <rww1d4@inf.elte.hu>
2023-11-09 22:02:57 +0100

    bug fixes

--- rest/script.js
+++ rest/script.js
@@ -1,12 +1,3 @@
-
-document.body.onload = () => {
-    document.querySelectorAll("nav a").forEach(function(e) {
-        if (e.href == window.location) {
-            e.classList.add("active");
-        }
-    })
-}
-
 // scroller animation **
 const scrollers = document.querySelectorAll(".scroller");
 
@@ -33,11 +24,18 @@ function addAnimation() {
 }
 // photo choosing
 window.onload = function() {
-let photos = ["photo-horizontal.jpg", "stylish.jpg"];
-let photo = document.querySelector("#photo");
-if (photo) {
-  photo.src = "rest/" + photos[Math.floor(Math.random() * photos.length)];
-}
+  let photos = ["photo-horizontal.jpg", "stylish.jpg"];
+  let photo = document.querySelector("#photo");
+  if (photo) {
+    photo.src = "rest/" + photos[Math.floor(Math.random() * photos.length)];
+  }
+
+  document.querySelectorAll("nav a").forEach(function(e) {
+    if (e.href == window.location) {
+        e.classList.add("active");
+    }
+  })
+  
 }
 
 //listing projects
@@ -59,47 +57,6 @@ fetch('./rest/data.json')
           `;
       }
   })
-.catch(error => console.error('Error:', error));
+.catch(error => error);
 
-
-// fetch('./rest/data.json')
-//     .then(response => response.json())
-//     .then(obj => obj.projects)
-    
-//     // .then(console.log);
-//     .then(projects => {
-//         for (let project of projects) {
-//             let projectDiv = document.createElement('div');
-//             projectDiv.className = 'col-md-4 box col-10';
-
-//             let projectTitle = document.createElement('h3');
-//             projectTitle.className = 'project-title';
-//             projectTitle.innerHTML = project.name;
-
-//             let projectDescription = document.createElement('p');
-//             projectDescription.className = 'project-description';
-//             projectDescription.innerHTML = project.description;
-
-//             let projectLink = document.createElement('a');
-//             projectLink.className = 'project-link';
-//             projectLink.href = project.link;
-//             projectLink.target = '_blank';
-//             projectLink.innerHTML = 'View Project';
-
-//             projectDiv.appendChild(projectTitle);
-//             projectDiv.appendChild(projectDescription);
-//             projectDiv.appendChild(projectLink);
-
-
-//             let wrapper = document.createElement('div');
-//             wrapper.className = 'col-12 d-flex justify-content-center';
-//             wrapper.appendChild(projectDiv);
-            
-
-
-//             document.querySelector('.projects').appendChild(wrapper);
-            
-//         }
-//     })
-//     .catch(error => console.error('Error:', error));
 
